e8836a3fc1fb28d9a1691c0be9cd22c0662f828b
Edvin Mocibob <edvin.mocibob@gmail.com>
2020-11-08 11:42:29 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <div>
+          <h1>Fib Calculator version 2</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </div>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
